e08ae11744842830c1a0436f2ce29cd7af939513
12-002-MDaffaHakimMatondang <muhammad.123140002@student.itera.ac.id>
2025-05-31 03:54:01 +0700

    ADDING LOGIC
    Logic MASTER DAN SIFU

---
+++ Algoritma The Master Sifu/run-bots.bat
@@ -0,0 +1,10 @@
+@echo off
+start cmd /c "python main.py --logic Sifu --email=test@email.com --name=stima --password=123456 --team etimo"
+start cmd /c "python main.py --logic Sifu --email=test1@email.com --name=stima1 --password=123457 --team etimo"
+start cmd /c "python main.py --logic Sifu --email=test2@email.com --name=stima2 --password=123458 --team etimo"
+start cmd /c "python main.py --logic Sifu --email=test40@email.com --name=stima4 --password=123460 --team etimo"
+
+start cmd /c "python main.py --logic Master --email=test4394@email.com --name=stima13 --password=123460 --team etimo"
+start cmd /c "python main.py --logic Master --email=test420@email.com --name=stima6 --password=123460 --team etimo"
+start cmd /c "python main.py --logic Master --email=test430@email.com --name=stima7 --password=123460 --team etimo"
+start cmd /c "python main.py --logic Master --email=test440@email.com --name=stima10 --password=123460 --team etimo"
